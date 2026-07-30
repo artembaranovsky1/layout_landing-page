455e71d974f749b625e3acbc76183b9a5814dd6b
artembaranovsky1 <baranovskyiartem2006@gmail.com>
2026-07-30 16:37:49 +0300

    fix bugs

---
+++ main.js
@@ -0,0 +1,7 @@
+const form = document.querySelector('#contacts-form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  form.reset();
+});
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const contactsForm = document.getElementById('contacts-form');
+
+contactsForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  contactsForm.reset();
+});
